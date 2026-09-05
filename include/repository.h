b509fcc63a341d4e428b2a3e16de0523df503f32
Rifat Khan <122586788+RifatNSU701@users.noreply.github.com>
2026-09-05 10:51:59 +0600

    Add repository thread-safety contract

--- include/repository.h
+++ include/repository.h
@@ -1,11 +1,15 @@
 #ifndef BOOKSHELF_REPOSITORY_H
 #define BOOKSHELF_REPOSITORY_H
 
+#include <pthread.h>
+
 #include "bookshelf_core.h"
 #include "storage_sqlite.h"
 
 typedef struct {
     BsSqliteStorage *storage;
+    pthread_mutex_t lock;
+    int initialized;
 } BsRepository;
 
 BsError bs_repository_open(BsRepository *repository, const char *path);
