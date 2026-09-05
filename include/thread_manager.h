2a7c55b921268c02e5d4a08308e6db1ffc223028
Rifat Khan <122586788+RifatNSU701@users.noreply.github.com>
2026-09-05 10:58:08 +0600

    Align thread manager with core API

--- include/thread_manager.h
+++ include/thread_manager.h
@@ -1,11 +1,9 @@
 #ifndef THREAD_MANAGER_H
 #define THREAD_MANAGER_H
 
-#include "inventory.h"
-#include "synchronization.h"
+#include "bookshelf_core.h"
 
-int run_organization_pipeline(Inventory *inv, SyncPrimitives *sync,
-                              int final_strategy);
+int run_organization_pipeline(BsInventory *inventory, int final_strategy);
 long run_race_demo(int use_mutex);
 
 #endif
